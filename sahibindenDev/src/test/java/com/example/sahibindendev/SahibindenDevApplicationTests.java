0e7d5c455709772ba6be3ea9ed95a2b62941f06b
kaankalan <kaankalan@outlook.com.tr>
2022-02-04 15:01:01 +0300

    re create projects base again

---
+++ sahibindenDev/src/test/java/com/example/sahibindendev/SahibindenDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sahibindendev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahibindenDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
